0b160ecc5895170c63b8dfa866917631bb7a83f4
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-11 16:50:13 -0800

    jumbotron, need to make more parts

--- memoryapp/src/App.js
+++ memoryapp/src/App.js
@@ -2,45 +2,66 @@ import React from "react";
 import FriendCard from "./components/FriendCard";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
+// import Venturebros from "./components/images"
 import friends from "./friends.json";
 
 import "./App.css";
 
 function App() {
   return (
-    
 
-    
+
+
 
     <Wrapper>
       <div>
-      <Navbar 
-      
-      />
+        <Navbar
 
+        // score={score}
+        />
       </div>
-      {/* <Navbar /> */}
       <h1 className="title">Friends List</h1>
       <FriendCard
-        name={friends[0].name}
         image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
       />
       <FriendCard
-        name={friends[1].name}
         image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
       />
       <FriendCard
-        name={friends[2].name}
         image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
+      />
+      <FriendCard
+        image={friends[3].image}
+      />
+      <FriendCard
+        image={friends[4].image}
+      />
+      <FriendCard
+        image={friends[5].image}
+      />
+      <FriendCard
+        image={friends[6].image}
+      />
+      <FriendCard
+        image={friends[7].image}
+      />
+      <FriendCard
+        image={friends[8].image}
+      />
+      <FriendCard
+        image={friends[9].image}
+      />
+      <FriendCard
+        image={friends[10].image}
+      />
+      <FriendCard
+        image={friends[11].image}
+      />
+      <FriendCard
+        image={friends[12].image}
       />
     </Wrapper>
-    
+
   );
 }
 
---
+++ memoryapp/src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-5">Venture Bros Clicky</h1>
+        <p className="lead">
+          Lets see if you can remember who the heroes and villans are!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
