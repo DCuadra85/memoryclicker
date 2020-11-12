ee3713fcf14dbfb8de5152f26dc54e191f7f7a5e
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-11 21:37:26 -0800

    deploy

--- memoryapp/src/App.js
+++ memoryapp/src/App.js
@@ -7,27 +7,60 @@ import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
 import "./App.css";
 
+class App extends Component {
+  // state = {
+  //   friends: friends,
+  //   score: 0,
+  //   highScore: 0
+  // };
 
-// state = {
-//   friends: friends,
-//   score: 0,
-//   highScore: 0
-// };
+  clickCharacter = (id) => {
 
-function App() {
-  return (
-    <Wrapper>
-      <div>
-        <Navbar
+    console.log(friends)
+    const clickCheck = this.state.friends.find((char) => char.id === id);
+    const shuffleFriends = (char) => {
+      char.sort(() => Math.random() - 0.5);
+    };
+    const resetClick = (list) =>
+      list.map((current) => ({ ...current, clicked: false }));
 
-        // score={this.state.score}
-        />
-      </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Jumbotron />
-      {/* {this.state.friends.map((friends) => (
+    //if the card has been clicked before, this resets them
+    if (clickCheck.clicked === true) {
+      resetClick(friends);
+      this.setState({ score: 0 })
+      return;
+    } else {
+      let friends1 = this.state.friends.map((char) => {
+        if (char.id === id) {
+          return {
+            ...char,
+            clicked: true
+            //sets true/false to recognize that the card has been clicked
+          };
+        }
+        return char;
+      });
+      this.setState({ friends: friends1, score: this.state.score + 1 });
+      console.log(this.state.score);
+      shuffleFriends(friends1);
+    }
+  };
+
+
+  render() {
+    return (
+      <Wrapper>
+        <div>
+          <Navbar
+
+          // score={this.state.score}
+          />
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Jumbotron />
+        {/* {this.state.friends.map((friends) => (
 
 
       ))} */}
@@ -38,9 +71,9 @@ function App() {
           clicked={friends.clicked}
         />
 
-    </Wrapper>
+      </Wrapper>
 
-  );
+    );
+  }
 }
-
 export default App;
